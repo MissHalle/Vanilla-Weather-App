806cfcbcf20255818a191efea7703c8182a3a7e9
MissHalle <100787203+MissHalle@users.noreply.github.com>
2022-03-11 13:52:55 -0600

    CSS and HTML
    added ids and indo from axios to wind, humidity, and city elements

--- src/app.js
+++ src/app.js
@@ -1,6 +1,15 @@
 function displayTemperature(response) {
+  let cityElement = document.querySelector("#city");
   let temperatureElement = document.querySelector("#temperature");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather(0).dscription;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "5a43febac9e08234e55ed941a9b78425";
