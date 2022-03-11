b949f79d8c60d8ccb91cb99207a2dba0211924d1
MissHalle <100787203+MissHalle@users.noreply.github.com>
2022-03-11 13:31:05 -0600

    CSS
    watching console.log for info on temp

--- src/app.js
+++ src/app.js
@@ -1,3 +1,8 @@
+function displayTemperature(response) {
+  console.log(response.data);
+}
+
 let apiKey = "5a43febac9e08234e55ed941a9b78425";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}`;
-console.log(apiUrl);
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
