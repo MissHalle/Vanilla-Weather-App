f6b16cc4991b480d50c46e9edc65bb0906ddc5c1
MissHalle <100787203+MissHalle@users.noreply.github.com>
2022-03-12 10:37:33 -0600

    adding a search bar

--- src/app.js
+++ src/app.js
@@ -44,8 +44,17 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
+function search(event) {
+  event.preventDefault();
+  let cityInputELement = document.querySelector("#city-input");
+  console.log(cityInputELement);
+}
+
 let apiKey = "5a43febac9e08234e55ed941a9b78425";
 let city = "New York";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", search);
