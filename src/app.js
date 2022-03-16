8428d133b8936baec652ceb23709b790e443f002
MissHalle <100787203+MissHalle@users.noreply.github.com>
2022-03-16 10:08:45 -0500

    JS updated conversion

--- src/app.js
+++ src/app.js
@@ -71,7 +71,6 @@ function displayCelsiusTemperature(event) {
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
 
-let celsiusTemperature = null;
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
