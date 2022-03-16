246fffd562ff08a089e226cfed1846f16bd66491
MissHalle <100787203+MissHalle@users.noreply.github.com>
2022-03-16 10:28:19 -0500

    ?? conversion didnt work, updating

--- src/app.js
+++ src/app.js
@@ -71,6 +71,8 @@ function displayCelsiusTemperature(event) {
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
 
+search("Seattle");
+
 let celsiusTemperature = null;
 
 let form = document.querySelector("#search-form");
@@ -78,8 +80,3 @@ form.addEventListener("submit", handleSubmit);
 
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
-
-let celsuisLink = document.querySelector("#celsui-link");
-celsuisLink.addEventListener("click", displayCelsiusTemperature);
-
-search("Seattle");
