ae168c3a7b85a55febb1ba2b13d4f2220dc26c85
MissHalle <100787203+MissHalle@users.noreply.github.com>
2022-03-20 13:20:13 -0500

    HTML into JS
    injected HTML of forecast into JS, added a new string days for forecast in JS, looping each day

--- src/app.js
+++ src/app.js
@@ -22,11 +22,28 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast {
-let forecastElement= document.querySelector("#forecast");
-forecastElement.innerHTML = "Forecast";
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
 
-   
+  let forecastHTML = `<div class="row">`;
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+  <div class="col-2">
+   <div class="weather-forecast-date">${day}</div>
+    <img src="http://openweathermap.org/img/wn/50d@2x.png" alt="" width="40" />
+    <div class="weather-forecast-temperatures">
+      <span class="weather-forecast-min-temperature"> 18° </span>
+      <span class="weather-forecast-max-temperature"> 70° </span>
+    </div>
+   </div>
+</div>
+`;
+  });
+  forecastHTML = forecastHTML + `</div`;
+  forecastElement.innerHTML = forecastHTML;
 }
 
 function displayTemperature(response) {
@@ -79,8 +96,6 @@ function displayCelsiusTemperature(event) {
 }
 
 let celsiusTemperature = null;
-
-
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
